ab00c041d93ff01734d7ab2cb3a8244fde31602d
Hoàng Phương <108871435+phuonghight@users.noreply.github.com>
2023-12-09 19:50:45 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Vue 3 Boilerplate
+
+## Commands
+
+1. Install dependencies.
+
+```bash
+yarn
+```
+
+2. Devlopment enviroment
+
+```bash
+yarn dev
+```
+
+3. Build
+
+```bash
+yarn build
+```
+
+## Features
+
+- **Framework**: [Vue](https://vuejs.org/)
+- **Router**: [Vue Router](https://router.vuejs.org/)
+- **State Management**: [Pinia](https://pinia.vuejs.org/)
+- **Style**: [Scss](https://sass-lang.com/)
+- **Icon**: [Tabler](https://tabler-icons.io/)
+- **Vite plugin**: [unplugin-auto-import](https://github.com/antfu/unplugin-auto-import), [unplugin-vue-components](https://github.com/antfu/unplugin-vue-components), [vite-plugin-pages](https://github.com/hannoeru/vite-plugin-pages), [vite-plugin-vue-layouts](https://github.com/JohnCampionJr/vite-plugin-vue-layouts)
+- **UI**: [NaiveUI](https://www.naiveui.com/)
---
+++ src/pages/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { useBreakpoints } from '@/composables/useBreakpoints';
+import useCounter from '@/composables/useCounter';
+
+const { width, device } = useBreakpoints();
+const { count, increment, decrement } = useCounter();
+
+onMounted(() => {
+  document.title = 'Vue 3 Boilerplate';
+});
+</script>
+
+<template>
+  <div class="home">
+    <div>This is Home page</div>
+    <div>Size: {{ width }}</div>
+    <div>Device: {{ device }}</div>
+
+    <div>Count: {{ count }}</div>
+
+    <div>
+      <n-button size="large" type="primary" @click="increment">Increment</n-button>
+      <n-button size="large" @click="decrement" style="margin-left: 2rem">Decrement</n-button>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home {
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
+<route lang="yaml">
+name: Home
+meta:
+  layout: default
+</route>
