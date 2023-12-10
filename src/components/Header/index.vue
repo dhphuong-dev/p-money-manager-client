0aa312c7f974a4c884f7d51434387112bd7bcf77
Hoàng Phương <108871435+phuonghight@users.noreply.github.com>
2023-12-10 13:57:17 +0700

    Feat/header footer (#2)
    * feat: completed set up project
    
    * feat: custom Header and Footer

--- src/components/Header/index.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <header style="text-align: center">Header</header>
-</template>
-
-<style scoped lang="scss"></style>
---
+++ src/components/PHeader/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <header style="padding-top: 2rem">
+    <n-space align="center" justify="space-between">
+      <n-space align="center" justify="space-between">
+        <slot name="back" />
+        <h2>{{ props.title }}</h2>
+      </n-space>
+      <slot name="function" />
+    </n-space>
+  </header>
+</template>
