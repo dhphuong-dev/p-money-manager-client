ac41a47af22e4bab8dc05dc8ebcb45bc6c38f12c
Hoàng Phương <108871435+phuonghight@users.noreply.github.com>
2024-01-23 15:40:44 +0700

    feat: simple home page (#19)

---
+++ src/components/RecentTransactions/index.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { getMyTransactions } from '@/api/transaction';
+import type { TransactionResponse } from '@/types/transaction.type';
+
+const props = defineProps<{
+  size?: number;
+}>();
+
+const message = useMessage();
+const recentTrans = ref<TransactionResponse[]>([]);
+
+onBeforeMount(async () => {
+  try {
+    const { data } = await getMyTransactions({ pageSize: props.size || 3 });
+    recentTrans.value = data.data.items;
+  } catch (err: any) {
+    if (!!err.response) {
+      message.error(err.response.data.message);
+    } else {
+      message.error(err.message);
+    }
+  }
+});
+</script>
+
+<template>
+  <div class="recent-transactions">
+    <n-space justify="space-between" align="center" class="container" style="margin-bottom: 1rem">
+      <p>Recent transactions</p>
+      <router-link class="see-all" to="/transaction">See all</router-link>
+    </n-space>
+
+    <p-transaction-link
+      v-for="tran in recentTrans"
+      :key="tran.id"
+      :transaction="tran"
+      class="transaction"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.recent-transactions {
+  padding-bottom: 7rem;
+  .see-all {
+    color: $primary;
+  }
+  .transaction {
+    margin: 1rem 0 !important;
+  }
+}
+</style>
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts">
 import { IconBellFilled } from '@tabler/icons-vue';
+
+import { getMyWallet } from '@/api/wallet';
+import type { WalletResponse } from '@/types/wallet.type';
+
+const message = useMessage();
+const myWallets = ref<WalletResponse[]>([]);
+
+onBeforeMount(async () => {
+  try {
+    const { data } = await getMyWallet();
+    myWallets.value = data.data;
+  } catch (err: any) {
+    if (!!err.response) {
+      message.error(err.response.data.message);
+    } else {
+      message.error(err.message);
+    }
+  }
+});
 </script>
 
 <template>
@@ -8,7 +27,55 @@ import { IconBellFilled } from '@tabler/icons-vue';
       <icon-bell-filled />
     </template>
   </p-header>
+
+  <p-card class="wallet-card">
+    <div class="wallet-view">
+      <n-space justify="space-between" align="center" class="wallet-view-title">
+        <p>My Wallets</p>
+        <router-link class="see-all" to="/wallet">See all</router-link>
+      </n-space>
+      <n-space
+        v-for="w in myWallets"
+        :key="w.id"
+        justify="space-between"
+        align="center"
+        class="wallets"
+      >
+        <n-space align="center">
+          <div class="wallet-img">
+            <img width="30" height="30" src="@\assets\images\wallet.png" alt="" />
+          </div>
+          <p>{{ w.name }}</p>
+        </n-space>
+        <p>${{ w.total }}</p>
+      </n-space>
+    </div>
+  </p-card>
+
+  <recent-transactions />
 </template>
+
+<style lang="scss" scoped>
+.see-all {
+  color: $primary;
+}
+.wallet-card {
+  margin: 2rem 0;
+  .wallet-view {
+    width: 100%;
+    &-title {
+      padding: 1rem 0;
+      border-bottom: 1px solid $gray;
+      p {
+        font-size: 20px;
+      }
+    }
+    .wallets {
+      padding: 1rem 0;
+    }
+  }
+}
+</style>
 
 <route lang="yaml">
 name: Home
